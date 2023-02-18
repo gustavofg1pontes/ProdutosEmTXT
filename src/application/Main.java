d15824720d09dd6f0c446725e1f6ee1452f5e5f7
Gustavo <gpontesf06@gmail.com>
2023-02-18 20:28:12 -0300

    Just a few tests using files classes in case I need to use it in the future (BufferedReader, BufferedWriter, FileReader, FileWriter)

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,9 +2,7 @@ package application;
 
 import entities.Product;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,29 +11,50 @@ import static java.lang.Integer.parseInt;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Product> products = new ArrayList<Product>();
+        ArrayList<Product> products = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Digite o caminho do arquivo: ");
         String path = sc.nextLine();
 
-        //try with resources (leitor)
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String linha = br.readLine();
-            //adiciona na lista de produtos e passa pra proxima linha
-            while (linha != null) {
-                System.out.println(linha);
-                String[] fields = linha.split(",");
-                String name = fields[0];
-                double price = parseDouble(fields[1]);
-                int quantity = parseInt(fields[2]);
-                products.add(new Product(name, price, quantity));
-                linha = br.readLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Erro");
-        }
+        while (true) {
+            //try with resources (leitor)
+            try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+                String linha = br.readLine();
+                //adiciona na lista de produtos e passa pra proxima linha
+                while (linha != null) {
+                    System.out.println(linha);
+                    String[] fields = linha.split(",");
+                    String name = fields[0];
+                    double price = parseDouble(fields[1]);
+                    int quantity = parseInt(fields[2]);
+                    products.add(new Product(name, price, quantity));
+                    linha = br.readLine();
+                }
 
+                System.out.println("Adicionar produto:");
+                System.out.print("NOME: ");
+                String nome = sc.nextLine();
+                System.out.print("PREÇO: ");
+                double preco = sc.nextDouble();
+                System.out.print("QUANTIDADE: ");
+                int quant = sc.nextInt();
+
+                products.add(new Product(nome, preco, quant));
+
+                try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+                    for(Product p : products){
+                        bw.write(p.getName() + "," +p.getPrice() + "," + p.getQuantity() );
+                        bw.newLine();
+                    }
+                }catch(IOException e){
+                    System.out.println("ERRO: " + e.getMessage());
+                }
+
+            } catch (IOException e) {
+                System.out.println("ERRO: " + e.getMessage());
+            }
+        }
 
     }
 }
